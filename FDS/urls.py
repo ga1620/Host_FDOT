c51ebbc21136a95add1a543c7a4be5ab4fe44f3d
Gauravrahangdale <gauravrahangdale27@gmail.com>
2022-06-15 11:06:59 +0530

    commit

--- FDS/urls.py
+++ FDS/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     # path('accounts/', include('django.contrib.auth.urls')),
 
     path('',include('Apps.homeApp.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
 
 if settings.DEBUG:
